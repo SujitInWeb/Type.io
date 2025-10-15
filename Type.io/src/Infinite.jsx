4345e33da6c99cc3734a91334d5feb7143b95bde
Sujit Ghosh <sujitghosh07.official@gmail.com>
2025-10-15 13:29:44 +0530

    designing

--- Type.io/src/Infinite.jsx
+++ Type.io/src/Infinite.jsx
@@ -1,3 +1,18 @@
 export default function Infinite() {
-  return <div className="h-screen bg-[#010409] text-white p-4 m-0">🚀 Infinite Mode Component</div>;
+  return (
+    <>
+      <div className="h-screen bg-[#010409] text-white p-4 m-0 flex justify-around flex-col items-center">
+          <div className="calculator border-[#3D444D]  bg-[#eef5fc27] w-full h-15 rounded-lg border-1 ">
+
+          </div>
+          <div className="paragraph flex justify-center items-center bg-[#eef5fc27] w-full h-100 border-[#3D444D] rounded-lg border-1 ">
+            <p className="text-center text-lg w-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem officia animi doloremque molestias, quae incidunt. Atque recusandae culpa iste excepturi dignissimos ipsa molestias deleniti est labore. Distinctio fugiat ab similique?
+            Odio odit eligendi commodi impedit itaque sed libero atque, fugiat deserunt laboriosam sequi numquam repellendus maxime reiciendis sunt. Mollitia deserunt dicta, nemo enim non explicabo error sint optio minima modi.
+            Delectus adipisci consequuntur eius et odit illo accusantium aspernatur earum exercitationem cum autem ipsum perspiciatis eveniet minima tempora magnam reprehenderit incidunt accusamus, aperiam minus repellat? Recusandae laudantium exercitationem dolor voluptate.
+            Non optio hic architecto autem, deserunt culpa. Sint, ipsum porro! Commodi debitis id culpa quod facere! Mollitia, perspiciatis! Placeat debitis dolore praesentium animi exercitationem. Eius et cum debitis ullam itaque!
+            Cum, aliquam. Cupiditate architecto odit mollitia doloremque sunt accusantium culpa officia, aut labore exercitationem consequuntur facere quos quidem, vitae dignissimos dolore. Fugit tenetur consectetur voluptate sit ipsam. Optio, consequatur vero!</p>
+          </div>
+      </div>
+    </>
+    );
 }
