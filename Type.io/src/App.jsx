5d65aab9d992f06b4970582174f96e07c50a8940
SujitInWeb <sujitghosh07.official@gmail.com>
2025-10-19 19:00:26 +0530

    fixed some bugs

--- Type.io/src/App.jsx
+++ Type.io/src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
     <>
       <div className="flex">
         {/* Sidebar controls mode switching */}
-        < Sidebar setActiveMode={setActiveMode} />
+        <Sidebar setActiveMode={setActiveMode} />
 
         <div className="flex-1">
           {activeMode === "infinite" && <InfiniteMode />}
