2654a3fe7a0de40e2f50560c85b3966124986f9d
Sujit Ghosh <sujitghosh07.official@gmail.com>
2025-11-24 22:58:03 +0530

    finished the project :)

--- Type.io/src/Sidebar.jsx
+++ Type.io/src/Sidebar.jsx
@@ -35,49 +35,52 @@ export default function Sidebar({ setActiveMode, isOpen, setIsOpen , isDarkMode 
         </h1>
         <button
           onClick={toggleTheme}
-          className="bg-gray-800/50 dark:bg-white/50 hover:bg-gray-800/80 dark:hover:bg-white/80 text-white dark:text-[#0D1117] lg:text-lg md:text-lg text-sm font-[Roboto] min-w-full p-3 border rounded-lg border-gray-300 dark:border-[#3D444D] my-2 cursor-pointer transition duration-280 flex items-center justify-center gap-2"
+          className="bg-black dark:bg-white/50 hover:bg-gray-800/80 dark:hover:bg-white/80 text-white dark:text-[#0D1117] lg:text-lg md:text-lg text-sm font-[Roboto] min-w-full p-3 border rounded-lg border-gray-300 dark:border-[#3D444D] my-2 cursor-pointer transition duration-280 flex items-center justify-center gap-2"
         >
           {isDarkMode ? (
             <>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
-                <path d="M12 18C8.68629 18 6 15.3137 6 12C6 8.68629 8.68629 6 12 6C15.3137 6 18 8.68629 18 12C18 15.3137 15.3137 18 12 18ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16ZM11 1H13V4H11V1ZM11 20H13V23H11V20ZM3.51472 4.92893L4.92893 3.51472L7.05025 5.63604L5.63604 7.05025L3.51472 4.92893ZM16.9497 18.364L18.364 16.9497L20.4853 19.0711L19.0711 20.4853L16.9497 18.364ZM19.0711 3.51472L20.4853 4.92893L18.364 7.05025L16.9497 5.63604L19.0711 3.51472ZM5.63604 16.9497L7.05025 18.364L4.92893 20.4853L3.51472 19.0711L5.63604 16.9497ZM23 11V13H20V11H23ZM4 11V13H1V11H4Z"/>
+                <path d="M10 7C10 10.866 13.134 14 17 14C18.9584 14 20.729 13.1957 21.9995 11.8995C22 11.933 22 11.9665 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C12.0335 2 12.067 2 12.1005 2.00049C10.8043 3.27098 10 5.04157 10 7ZM4 12C4 16.4183 7.58172 20 12 20C15.0583 20 17.7158 18.2839 19.062 15.7621C18.3945 15.9187 17.7035 16 17 16C12.0294 16 8 11.9706 8 7C8 6.29648 8.08133 5.60547 8.2379 4.938C5.71611 6.28423 4 8.9417 4 12Z"/>
+                
               </svg>
-              <span>Light Mode</span>
+              
             </>
           ) : (
             <>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
-                <path d="M10 7C10 10.866 13.134 14 17 14C18.9584 14 20.729 13.1957 21.9995 11.8995C22 11.933 22 11.9665 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C12.0335 2 12.067 2 12.1005 2.00049C10.8043 3.27098 10 5.04157 10 7ZM4 12C4 16.4183 7.58172 20 12 20C15.0583 20 17.7158 18.2839 19.062 15.7621C18.3945 15.9187 17.7035 16 17 16C12.0294 16 8 11.9706 8 7C8 6.29648 8.08133 5.60547 8.2379 4.938C5.71611 6.28423 4 8.9417 4 12Z"/>
+                <path d="M12 18C8.68629 18 6 15.3137 6 12C6 8.68629 8.68629 6 12 6C15.3137 6 18 8.68629 18 12C18 15.3137 15.3137 18 12 18ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16ZM11 1H13V4H11V1ZM11 20H13V23H11V20ZM3.51472 4.92893L4.92893 3.51472L7.05025 5.63604L5.63604 7.05025L3.51472 4.92893ZM16.9497 18.364L18.364 16.9497L20.4853 19.0711L19.0711 20.4853L16.9497 18.364ZM19.0711 3.51472L20.4853 4.92893L18.364 7.05025L16.9497 5.63604L19.0711 3.51472ZM5.63604 16.9497L7.05025 18.364L4.92893 20.4853L3.51472 19.0711L5.63604 16.9497ZM23 11V13H20V11H23ZM4 11V13H1V11H4Z"/>
               </svg>
-              <span>Dark Mode</span>
+              
             </>
           )}
         </button>
         
         <button 
-          className="bg-white/50 lg:text-lg md:text-lg text-sm hover:bg-white/80 font-[Roboto] font-normal text-[#0D1117]  min-w-full p-3 border rounded-lg border-[#3D444D] my-2 hover:border-[#3D444D] hover:text-[#0D1117] cursor-pointer transition duration-280"
+          className="bg-white/80 flex justify-center items-center dark:bg-black dark:hover:border-black dark:text-white  dark:hover:bg-white dark:hover:text-black lg:text-lg md:text-lg text-sm hover:bg-white font-[Roboto] font-normal text-black  min-w-full p-3 border rounded-lg border-white my-2 hover:border-white hover:text-black cursor-pointer transition duration-280"
           onClick={() => {
             setActiveMode("infinite");
             setIsOpen(false);
           }}
         >
-          ∞ Infinite Mode
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 pr-1"><path d="M3 12C3 10.067 4.567 8.5 6.5 8.5C7.7035 8.5 8.51959 8.9338 9.19914 9.61336C9.9255 10.3397 10.4851 11.3322 11.1258 12.4856L11.1595 12.5462C11.7605 13.6283 12.4431 14.8573 13.3866 15.8009C14.3946 16.8088 15.7035 17.5 17.5 17.5C20.5376 17.5 23 15.0376 23 12C23 8.96243 20.5376 6.5 17.5 6.5C15.8394 6.5 14.3508 7.2359 13.3423 8.39937C13.7887 9.05406 14.1574 9.70577 14.464 10.2574C15.0681 9.20718 16.2014 8.5 17.5 8.5C19.433 8.5 21 10.067 21 12C21 13.933 19.433 15.5 17.5 15.5C16.2965 15.5 15.4804 15.0662 14.8009 14.3866C14.0745 13.6603 13.5149 12.6678 12.8742 11.5144L12.8405 11.4538C12.2395 10.3717 11.5569 9.14265 10.6134 8.19914C9.60541 7.1912 8.2965 6.5 6.5 6.5C3.46243 6.5 1 8.96243 1 12C1 15.0376 3.46243 17.5 6.5 17.5C8.16056 17.5 9.64923 16.7641 10.6577 15.6006C10.2113 14.9459 9.84262 14.2942 9.53605 13.7426C8.93194 14.7928 7.79856 15.5 6.5 15.5C4.567 15.5 3 13.933 3 12Z"></path></svg>
+           Infinite Mode
         </button>
         
         <button 
-          className="bg-white/50 lg:text-lg md:text-lg text-sm hover:bg-white/80 font-[Roboto] font-normal text-[#0D1117]  min-w-full p-3 border rounded-lg border-[#3D444D] my-2 hover:border-[#3D444D] hover:text-[#0D1117] cursor-pointer transition duration-280"
+          className="bg-white/80 flex justify-center items-center dark:bg-black dark:hover:border-black dark:text-white  dark:hover:bg-white dark:hover:text-black  lg:text-lg md:text-lg text-sm hover:bg-white font-[Roboto] font-normal text-black  min-w-full p-3 border rounded-lg border-white my-2 hover:border-white hover:text-[#0D1117] cursor-pointer transition duration-280"
           onClick={() => {
             setActiveMode("calculator");
             setIsOpen(false);
           }}
         >
-         ⏱ Calculator Mode
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6 pr-1"><path d="M17.6177 5.9681L19.0711 4.51472L20.4853 5.92893L19.0319 7.38231C20.2635 8.92199 21 10.875 21 13C21 17.9706 16.9706 22 12 22C7.02944 22 3 17.9706 3 13C3 8.02944 7.02944 4 12 4C14.125 4 16.078 4.73647 17.6177 5.9681ZM12 20C15.866 20 19 16.866 19 13C19 9.13401 15.866 6 12 6C8.13401 6 5 9.13401 5 13C5 16.866 8.13401 20 12 20ZM11 8H13V14H11V8ZM8 1H16V3H8V1Z"></path></svg>
+            Calculator Mode
         </button>
         
         <div className="flex justify-center w-full mt-auto mb-4">
           <div 
             onClick={() => window.open("https://x.com/SujitInweb", "_blank")} 
-            className="md:h-12 md:w-12 xl:h-12 xl:w-12 h-10 w-10 p-1 mx-4 bg-white/50 flex justify-center items-center rounded-lg dark:border cursor-pointer hover:bg-white/80 transition"
+            className="md:h-12 md:w-12 xl:h-12 xl:w-12 h-10 w-10 p-1 mx-4 bg-white/80 flex justify-center items-center rounded-lg dark:border cursor-pointer hover:bg-white transition"
           >
             <svg 
               xmlns="http://www.w3.org/2000/svg" 
@@ -90,7 +93,7 @@ export default function Sidebar({ setActiveMode, isOpen, setIsOpen , isDarkMode 
           </div>
           <button 
             onClick={() => window.open("https://github.com/SujitInWeb/Type.io", "_blank")} 
-            className="md:h-12 md:w-12 xl:h-12 xl:w-12 h-10 w-10 p-1 mx-4 bg-white/50 flex justify-center items-center rounded-lg dark:border cursor-pointer hover:bg-white/80 transition"
+            className="md:h-12 md:w-12 xl:h-12 xl:w-12 h-10 w-10 p-1 mx-4 bg-white/80 flex justify-center items-center rounded-lg dark:border cursor-pointer hover:bg-white transition"
           >
             <svg 
               xmlns="http://www.w3.org/2000/svg" 
